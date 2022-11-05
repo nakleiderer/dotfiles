b725eaae553b191667e96d8f946ee3b22dd277b8
Nicolas Kleiderer <4278631+nakleiderer@users.noreply.github.com>
2022-11-04 22:47:59 -0500

    update dock

--- macos/dock.sh
+++ macos/dock.sh
@@ -2,11 +2,12 @@
 
 dockutil --no-restart --remove all
 dockutil --no-restart --add "/System/Applications/Launchpad.app"
-dockutil --no-restart --add "/System/Applications/System Preferences.app"
+dockutil --no-restart --add "/System/Applications/System Settings.app"
 dockutil --no-restart --add "/System/Applications/Utilities/Terminal.app"
 dockutil --no-restart --add "/Applications/Firefox.app"
+dockutil --no-restart --add "/Applications/Google Chrome.app"
 dockutil --no-restart --add "/Applications/Notion.app"
-dockutil --no-restart --add "~/Applications/Visual Studio Code.app"
+dockutil --no-restart --add "/Applications/Visual Studio Code.app"
 dockutil --no-restart --add "/System/Applications/Calendar.app"
 dockutil --no-restart --add "/System/Applications/Notes.app"
 dockutil --no-restart --add "/System/Applications/Messages.app"
